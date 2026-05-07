ec5e89cfa39c5a50da1750783509e52b59d80363
EthanKKuntz <139951520+EthanKKuntz@users.noreply.github.com>
2026-05-06 21:21:34 -0700

    apply custom design

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ethan K Kuntz';
+export const SITE_DESCRIPTION = 'Personal site of Ethan K Kuntz';
